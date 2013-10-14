1faddef6f9f598e556714d091b89cdc945d7807e
Dean Poulin <dean.poulin@gmail.com>
2013-10-13 20:37:39 -0400

    Created the sample package.

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# 0.0.1 (2013-10-13)
+
+Created the test package.
+
+## Features
+* simple data grid component.
+
---
+++ src/alert/data-grid.js
@@ -0,0 +1,15 @@
+angular.module("hms.components.alert", []).directive('alert', function () {
+  return {
+    restrict:'EA',
+    templateUrl:'template/alert/alert.html',
+    transclude:true,
+    replace:true,
+    scope: {
+      type: '=',
+      close: '&'
+    },
+    link: function(scope, iElement, iAttrs, controller) {
+      scope.closeable = "close" in iAttrs;
+    }
+  };
+});
